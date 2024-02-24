030b16385d7028d2c1dccaf1d48685b39e363538
michael_ubuntu <michalrenyorsar@gmail.com>
2024-02-24 22:34:08 +0200

    24-02/3

--- Simple Dash Example/main.py
+++ Simple Dash Example/main.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
@@ -28,31 +31,45 @@ app = dash.Dash(title='Simple Dash Example', prevent_initial_callbacks=True, ser
 # Створення шахматної дошки
 def createNewChessboard():
     chessboard = []
-    for i in range(8):
-        row = []
-        for j in range(8):
-            color = "white" if (i + j) % 2 == 0 else "black"
-            cell = html.Button(
-                id=f"{i}_{j}",
-                style={'backgroundColor': color,
-                       'width': '50px',
-                       'height': '50px',
-                       'color': 'red',
-                       'font-size': '8px'
-                       },
-                children=f'{newChessBoard[i][j]}'
-            )
-            row.append(cell)
-        chessboard.append(html.Div(row, className='chess-row'))
-    return chessboard
+    with open(f'{lib}current_board.txt', 'r') as file:
+        curBoarReadRow = file.readlines()
 
-app.layout = html.Div(
-    [
-        html.H1("Шахматна дошка"),
-        html.Div(createNewChessboard(), className='chessboard'),
-        html.Plaintext('Запсиувач ходів [пусто]', id="move_history")
-    ]
-)
+        for i in range(8):
+            row = []
+            for j in range(8):
+                color = "white" if (i + j) % 2 == 0 else "black"
+                cell = html.Button(
+                    id=f"{i}_{j}",
+                    style={'backgroundColor': color,
+                           'width': '50px',
+                           'height': '50px',
+                           'color': 'red',
+                           'font-size': '8px'
+                           },
+                    children=f'{curBoarReadRow[i][3 * j:3 * j + 2]}'
+                )
+                row.append(cell)
+            chessboard.append(html.Div(row, className='chess-row'))
+        return chessboard
+
+
+def my_loop():
+    while True:
+        app.layout = html.Div(
+            [
+                html.H1("Шахматна дошка"),
+                html.Div(createNewChessboard(), className='chessboard'),
+                html.Plaintext('Запсиувач ходів [пусто]', id="move_history")
+            ]
+        )
+        time.sleep(1000)
+
+loop_thread = threading.Thread(target=my_loop)
+loop_thread.start()
+
+
+
+
 
 
 
@@ -65,12 +82,20 @@ for i in range(8):
 
         )
         def record_move(n_clicks, row=i, col=j):
-            with open("moves.txt", "a") as f:
-                f.write(f"({row}, {col}) - {datetime.datetime.now()}\n")
-            curBoar = open(f'{lib}current_board.txt').read().splitlines()
+            with open("moves.txt", "a") as moves:
+                moves.write(f"({row}, {col}) - {datetime.datetime.now()}\n")
 
-            selected_chars = curBoar[row][3 * col:3 * col + 2]
-            return "Move recorded successfully!", ':)'+selected_chars
+            with open(f'{lib}current_board.txt', 'r') as file:
+                curBoarReadRow = file.readlines()
+                curBoarReadRow[row] = curBoarReadRow[row][:3 * col] + ":) " + curBoarReadRow[row][3 * col + 3:]
+                with open(f'{lib}current_board.txt', 'w') as file:
+                    file.writelines(curBoarReadRow)
+                selected_chars = curBoarReadRow[row][3 * col:3 * col + 2]
+                if(selected_chars==':) '):
+                    selected_chars=':P'
+
+            return "Move recorded successfully!", f"{selected_chars}"
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port='5902')
+    app.run_server(debug=True, port='1984')
